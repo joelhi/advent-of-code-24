723b83f4ee84d0fb4bf2d423a7b3949676fda928
joelhi <d.j.hilmersson@gmail.com>
2024-12-01 18:56:49 +0000

    Formatting

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use problems::*;
 fn main() {
     let args = std::env::args().collect::<Vec<String>>();
     let day = &args[1].parse::<i32>().unwrap();
-    
+
     println!("Running day {}", day);
     match solve_day(day) {
         Ok(result) => {
@@ -14,13 +14,12 @@ fn main() {
             for (i, val) in result.iter().enumerate() {
                 println!("{}: {}", i, val);
             }
-        },
-        Err(err) => println!("Failed with error: {}", err)
+        }
+        Err(err) => println!("Failed with error: {}", err),
     }
-
 }
 
-fn solve_day(day: &i32)->Result<Vec<u32>,String>{
+fn solve_day(day: &i32) -> Result<Vec<u32>, String> {
     let data = match read_input_for_day(day) {
         Ok(data) => data,
         Err(err) => return Err(err.to_string()),
@@ -29,7 +28,9 @@ fn solve_day(day: &i32)->Result<Vec<u32>,String>{
     match day {
         1 => Ok(day1::solve(&data)),
         ..=25 => Err(format!("Day {} is not solved yet", day)),
-        _ => Err(format!("Number {} is not a valid day for the calendar...", day)),
+        _ => Err(format!(
+            "Number {} is not a valid day for the calendar...",
+            day
+        )),
     }
-    
 }

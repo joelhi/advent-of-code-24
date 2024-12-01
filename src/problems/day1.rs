94a32df9bd9ce6c51bce3f44088a1ad91bd91ff7
joelhi <d.j.hilmersson@gmail.com>
2024-12-01 20:20:41 +0000

    Day 1 part 2

--- src/problems/day1.rs
+++ src/problems/day1.rs
@@ -1,31 +1,48 @@
+use std::collections::HashMap;
+
 /// Solve the problem for day one, given the provided data.
 pub fn solve(data: &Vec<String>) -> Result<Vec<u32>, String> {
-    let answer_part_1 = solve_part_1(data)?;
-    //let answer_part_2 = solve_part_2(data)?;
+    let (mut first_list, mut second_list) = read_input_into_lists(data)?;
 
-    Ok(vec![answer_part_1, 0])
+    // Part 1
+    first_list.sort();
+    second_list.sort();
+
+    let mut answer_part_1 = 0;
+    for (first, second) in first_list.iter().zip(&second_list) {
+        answer_part_1 += first.abs_diff(*second);
+    }
+
+    // Part 2
+    let mut matches: HashMap<u32, u32> = HashMap::with_capacity(first_list.len());
+    for &num in second_list.iter() {
+        *matches.entry(num).or_insert(0) += 1;
+    }
+
+    let mut answer_part_2 = 0;
+    for &num in first_list.iter(){
+        let num_matches = *matches.get(&num).unwrap_or(&0);
+        answer_part_2 += num * num_matches;
+    }
+
+    Ok(vec![answer_part_1, answer_part_2])
 }
 
-fn solve_part_1(data: &Vec<String>) -> Result<u32, String> {
+// Parse the text data into two lists of u32s.
+fn read_input_into_lists(data: &Vec<String>)->Result<(Vec<u32>, Vec<u32>), String> {
     let mut first_list: Vec<u32> = Vec::with_capacity(data.len());
     let mut second_list: Vec<u32> = Vec::with_capacity(data.len());
+
     for s in data.iter() {
         let (first_val, second_val) = split_entry(s)?;
         first_list.push(first_val);
         second_list.push(second_val);
     }
 
-    first_list.sort();
-    second_list.sort();
-
-    let mut result = 0;
-    for (first, second) in first_list.iter().zip(second_list) {
-        result += first.abs_diff(second);
-    }
-
-    Ok(result)
+    Ok((first_list, second_list))
 }
 
+// Split on entry into two values
 fn split_entry(s: &String) -> Result<(u32, u32), String> {
     let parts: Vec<&str> = s.split("   ").collect();
 
@@ -43,10 +60,6 @@ fn split_entry(s: &String) -> Result<(u32, u32), String> {
 
     Ok((first_value, second_value))
 }
-
-fn solve_part_2(data: &Vec<String>) -> Result<u32, String> {
-    Err("Day 1 part 2 not solved yet".to_string())
-}
 #[cfg(test)]
 mod tests {
     use crate::{day1::solve, read_input_for_day};
@@ -56,6 +69,8 @@ mod tests {
         // Wip
         let result =
             solve(&read_input_for_day(&1).expect("Expect the data file to be there.")).unwrap();
+        
         assert_eq!(2367773, result[0]);
+        assert_eq!(21271939, result[1])
     }
 }

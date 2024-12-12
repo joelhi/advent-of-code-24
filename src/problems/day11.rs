0c3373d36cdc976b9f679823c928f3741620389b
joelhi <d.j.hilmersson@gmail.com>
2024-12-12 00:00:23 +0000

    Part 11

--- src/problems/day11.rs
+++ src/problems/day11.rs
@@ -1,4 +1,4 @@
-use std::collections::{HashMap, HashSet};
+use std::collections::HashMap;
 
 use super::utils;
 
@@ -6,11 +6,19 @@ use super::utils;
 pub fn solve(input_data: &[String]) -> Result<Vec<u64>, String> {
     let mut stone_map = parse_stone_map(&input_data[0])?;
 
+    // Part 1
     for _ in 0..25 {
         map_stones(&mut stone_map)?;
     }
+    let result_part_1 = stone_map.iter().map(|(_, c)| c).sum();
 
-    Ok(vec![])
+    // Continue for part 2
+    for _ in 0..50 {
+        map_stones(&mut stone_map)?;
+    }
+    let result_part_2 = stone_map.iter().map(|(_, c)| c).sum();
+
+    Ok(vec![result_part_1, result_part_2])
 }
 
 /// Maps the initial stones into a map, with the count of each number
@@ -24,28 +32,20 @@ fn parse_stone_map(input: &str) -> Result<HashMap<u64, u64>, String> {
     Ok(unique_stones)
 }
 
+/// Compute a new map from the stones in the input map, by applying the rules
 fn map_stones(stone_map: &mut HashMap<u64, u64>) -> Result<(), String> {
-    let keys: HashSet<u64> = stone_map.keys().copied().collect();
-    for val in keys {
+    let mut new_map = HashMap::with_capacity(stone_map.len());
+    for (&val, &count) in stone_map.iter() {
         if val == 0 {
-            if let Some(&count) = stone_map.get(&0) {
-                stone_map.remove(&0);
-                *stone_map.entry(1).or_insert(0) += count;
-            }
+            *new_map.entry(1).or_insert(0) += count;
         } else if let Some((a, b)) = split_val(val) {
-            if let Some(&count) = stone_map.get(&val) {
-                stone_map.remove(&val);
-                *stone_map.entry(a).or_insert(0) += count;
-                *stone_map.entry(b).or_insert(0) += count;
-            }
+            *new_map.entry(a).or_insert(0) += count;
+            *new_map.entry(b).or_insert(0) += count;
         } else {
-            if let Some(&count) = stone_map.get(&val) {
-                stone_map.remove(&val);
-                *stone_map.entry(val * 2024).or_insert(0) += count;
-            }
+            *new_map.entry(val * 2024).or_insert(0) += count;
         }
     }
-
+    *stone_map = new_map;
     Ok(())
 }
 
@@ -57,14 +57,15 @@ fn split_val(val: u64) -> Option<(u64, u64)> {
         return None;
     }
 
-    let mid = len / 2;
-
-    // Split the string into two halves
-    let (first_half, second_half) = num_str.split_at(mid);
-
-    // Convert the halves back to u64
-    let first_half_num = first_half.parse::<u64>().unwrap_or(0);
-    let second_half_num = second_half.parse::<u64>().unwrap_or(0);
+    let (first_half, second_half) = num_str.split_at(len / 2);
+    let first_half_num = first_half.parse::<u64>().expect(&format!(
+        "Should be able to parse {} into a number",
+        first_half
+    ));
+    let second_half_num = second_half.parse::<u64>().expect(&format!(
+        "Should be able to parse {} into a number",
+        second_half
+    ));
 
     Some((first_half_num, second_half_num))
 }
@@ -86,7 +87,7 @@ mod tests {
         let result = solve(&data).unwrap();
         assert_eq!(
             55312, result[0],
-            "Result for part 1 example should be 36 but was {}",
+            "Result for part 1 example should be 55312 but was {}",
             result[0]
         );
     }
@@ -96,7 +97,7 @@ mod tests {
         let result =
             solve(&read_input_for_day(&11).expect("Expect the data file to be there.")).unwrap();
 
-        assert_eq!(0, result[0]);
-        assert_eq!(0, result[1]);
+        assert_eq!(229043, result[0]);
+        assert_eq!(272673043446478, result[1]);
     }
 }
